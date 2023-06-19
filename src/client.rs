b027b761fd0396ea24572e7c33836e3ba5389174
Jaehyeon Park <skystar@skystar.dev>
2023-06-19 18:05:14 +0900

    basic func

---
+++ src/client.rs
@@ -0,0 +1,147 @@
+use reqwest::header::USER_AGENT;
+use scraper::{Html, Selector};
+use thiserror::Error;
+
+pub struct UmobileClient {
+    client: reqwest::Client,
+}
+
+#[derive(Error, Debug)]
+pub enum ClientError {
+    #[error("login failed")]
+    LoginError,
+    #[error("usage fetch error")]
+    UsageFetchError,
+    #[error("bill fetch error")]
+    BillFetchError,
+}
+
+const USER_AGENT_STR: &str =
+    "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0";
+
+#[derive(Default, Debug)]
+pub struct Usage {
+    pub mobile_data_used: Option<f64>,
+    pub call_used: Option<f64>,
+    pub sms_used: Option<f64>,
+}
+
+impl UmobileClient {
+    pub async fn new_with_id_password(
+        id: impl AsRef<str>,
+        password: impl AsRef<str>,
+    ) -> Result<Self, ClientError> {
+        let id = id.as_ref().to_string();
+        let password = password.as_ref().to_string();
+
+        let client = reqwest::Client::builder()
+            .cookie_store(true)
+            .build()
+            .map_err(|_| ClientError::LoginError)?;
+
+        let form = reqwest::multipart::Form::new()
+            .text("username", id)
+            .text("password", password)
+            .text("loginMode", "idpw")
+            .text("nextPage", "/")
+            .text("autoLogin", "false");
+
+        let res = client
+            .post("https://www.uplusumobile.com/login-act")
+            .header(USER_AGENT, USER_AGENT_STR)
+            .multipart(form)
+            .send()
+            .await
+            .map_err(|_| ClientError::LoginError)?;
+
+        if res.status() != reqwest::StatusCode::OK {
+            return Err(ClientError::LoginError);
+        }
+
+        Ok(Self { client })
+    }
+
+    pub async fn get_realtime_usage(self) -> Result<Usage, ClientError> {
+        let html = self
+            .client
+            .get("https://www.uplusumobile.com/my/usage/realTime")
+            .send()
+            .await
+            .map_err(|_| ClientError::UsageFetchError)?
+            .text()
+            .await
+            .map_err(|_| ClientError::UsageFetchError)?;
+
+        let doc = Html::parse_document(&html);
+
+        let item_box_sel = Selector::parse("section.box-usage-wrap").unwrap();
+        let title_sel = Selector::parse("strong.usage-tit").unwrap();
+        let usage_box_sel = Selector::parse("div.usage-amount").unwrap();
+
+        let mut usage = Usage::default();
+
+        for item_box in doc.select(&item_box_sel) {
+            let title = item_box.select(&title_sel).next();
+            let title: String = if title.is_some() {
+                title.unwrap()
+            } else {
+                continue;
+            }
+            .text()
+            .collect();
+
+            let usage_box = item_box.select(&usage_box_sel).next();
+            let usage_text: String = if let Some(b) = usage_box {
+                b.text().collect()
+            } else {
+                continue;
+            };
+            let usage_text = usage_text.replace("남음", "").replace("사용", "");
+
+            if title.contains("데이터") {
+                let usage_text = usage_text.replace("GB", "");
+                let (_, used) = match usage_text.split_once("/") {
+                    Some(s) => s,
+                    None => continue,
+                };
+
+                let used = used
+                    .trim()
+                    .parse::<f64>()
+                    .map_err(|_| ClientError::UsageFetchError)?;
+
+                usage.mobile_data_used = Some(used);
+            } else if title.contains("음성통화") {
+                let usage_text = usage_text.replace("분", "");
+                let (_, used) = match usage_text.split_once("/") {
+                    Some(s) => s,
+                    None => continue,
+                };
+
+                let used = used
+                    .trim()
+                    .parse::<f64>()
+                    .map_err(|_| ClientError::UsageFetchError)?;
+
+                usage.call_used = Some(used);
+            } else if title.contains("메시지") {
+                let usage_text = usage_text.replace("건", "");
+                let (_, used) = match usage_text.split_once("/") {
+                    Some(s) => s,
+                    None => continue,
+                };
+
+                let used = used
+                    .trim()
+                    .parse::<f64>()
+                    .map_err(|_| ClientError::UsageFetchError)?;
+
+                usage.call_used = Some(used);
+            } else {
+                continue;
+            }
+        }
+
+        return Ok(usage);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+mod client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = client::UmobileClient::new_with_id_password("id", "password").await?;
+
+    Ok(())
 }
